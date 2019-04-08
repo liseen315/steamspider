c45a2051e7a4654018e1ba098c225f17382dcb63
刘松 <ls@btctrade.com>
2019-04-08 16:41:51 +0800

    add tags

--- steamspider/items.py
+++ steamspider/items.py
@@ -10,6 +10,10 @@ from peewee import *
 
 db = MySQLDatabase("steam_vgfuns", host='127.0.0.1', port=3306, user='root', passwd='Liseen315song', charset='utf8')
 
+class TagsItem(scrapy.Item):
+    tag_name = scrapy.Field()
+    tag_value = scrapy.Field()
+
 
 class TopSellersItem(scrapy.Item):
     app_id = scrapy.Field()
@@ -30,7 +34,15 @@ class AppDetailItem(TopSellersItem):
     developers = scrapy.Field()
     popular_tags = scrapy.Field()
     game_area_metascore = scrapy.Field()
-    game_types = scrapy.Field()
+
+
+class Tag(Model):
+    tag_name = CharField(verbose_name='标签名称')
+    tag_value = IntegerField(verbose_name='标签值')
+
+    class Meta:
+        database = db
+
 
 class TopSellers(Model):
     app_id = CharField(verbose_name='app唯一id', null=False, unique=True)
@@ -49,4 +61,9 @@ class PopularNews(TopSellers):
     pass
 
 class AppDetail(TopSellers):
-    pass
+    des = TextField(verbose_name='描述',null=True)
+    highlight_movie = CharField(verbose_name='焦点图视频')
+    screenshot = TextField(verbose_name='焦点图地址列表')
+    developers = CharField(verbose_name='开发商')
+    popular_tags = CharField(verbose_name='热门标签')
+    game_area_metascore = CharField(verbose_name='meta评分')
---
+++ steamspider/spiders/tags.py
@@ -0,0 +1,28 @@
+from scrapy import Spider, Request
+from steamspider.items import TagsItem
+
+
+class TagSpider(Spider):
+    name = 'apptags'
+    allowed_domains = ['store.steampowered.com']
+
+    def __init__(self, *args, **kwargs):
+        super(TagSpider, self).__init__(*args, **kwargs)
+
+        self.page_url = 'https://store.steampowered.com/search/?l=schinese&category1=998,21,10'
+        self.current_pagenum = 1
+        self.total_apps = None
+        self.total_pagenum = None
+        self.search_url = '{url}&page={pagenum}'
+
+    def start_requests(self):
+        yield Request(url=self.search_url.format(url=self.page_url, pagenum=self.current_pagenum),
+                      callback=self.parse_tags)
+
+    def parse_tags(self, response):
+        tag_list = response.xpath('//div[contains(@data-param,"tags")]').extract()
+        for tag_item in tag_list:
+            item = TagsItem()
+            item['tag_name'] = tag_item.xpath('@data-loc').extract_first()
+            item['tag_value'] = tag_item.xpath('@data-value').extract_first()
+            yield item
