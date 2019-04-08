ea4fa407d33f31b7e5a42e8615b2341fce2f99c0
刘松 <ls@btctrade.com>
2019-04-08 17:11:34 +0800

    fix pipline

--- steamspider/pipelines.py
+++ steamspider/pipelines.py
@@ -10,45 +10,52 @@ from .items import TopSellers, PopularNews
 
 class MySQLPipeline(object):
 
+    def __init__(self):
+        self.switch = {'topsellers': self.option_topsellers, 'poplularnew': self.option_popularnew}
+
+    def option_topsellers(self, item):
+        if TopSellers.table_exists() == False:
+            TopSellers.create_table()
+
+        try:
+            target_app = TopSellers.get(TopSellers.app_id == item['app_id'])
+            if target_app.discount != item['discount']:
+                TopSellers.update(discount=item['discount']).where(TopSellers.app_id == item['app_id']).execute()
+
+            if target_app.final_price != item['final_price']:
+                TopSellers.update(final_price=item['final_price']).where(
+                    TopSellers.app_id == item['app_id']).execute()
+
+        except TopSellers.DoesNotExist:
+            TopSellers.create(app_id=item['app_id'], name=item['name'], thumb_url=item['thumb_url'],
+                              released=item['released'], discount=item['discount'], final_price=item['final_price'],
+                              origin_price=item['origin_price'])
+
+    def option_popularnew(self, item):
+        if PopularNews.table_exists() == False:
+            PopularNews.create_table()
+
+        try:
+            target_app = PopularNews.get(PopularNews.app_id == item['app_id'])
+
+            if target_app.discount != item['discount']:
+                PopularNews.update(discount=item['discount']).where(PopularNews.app_id == item['app_id']).execute()
+
+            if target_app.final_price != item['final_price']:
+                PopularNews.update(final_price=item['final_price']).where(
+                    PopularNews.app_id == item['app_id']).execute()
+
+        except PopularNews.DoesNotExist:
+            PopularNews.create(app_id=item['app_id'], name=item['name'], thumb_url=item['thumb_url'],
+                               released=item['released'], discount=item['discount'],
+                               final_price=item['final_price'],
+                               origin_price=item['origin_price'])
+
     def process_item(self, item, spider):
 
-        if (spider.name == 'topsellers'):
-
-            if TopSellers.table_exists() == False:
-                TopSellers.create_table()
-
-            try:
-                target_app = TopSellers.get(TopSellers.app_id == item['app_id'])
-                if target_app.discount != item['discount']:
-                    TopSellers.update(discount=item['discount']).where(TopSellers.app_id == item['app_id']).execute()
-
-                if target_app.final_price != item['final_price']:
-                    TopSellers.update(final_price=item['final_price']).where(
-                        TopSellers.app_id == item['app_id']).execute()
-
-            except TopSellers.DoesNotExist:
-                TopSellers.create(app_id=item['app_id'], name=item['name'], thumb_url=item['thumb_url'],
-                                  released=item['released'], discount=item['discount'], final_price=item['final_price'],
-                                  origin_price=item['origin_price'])
-
-        elif (spider.name == 'poplularnew'):
-            if PopularNews.table_exists() == False:
-                PopularNews.create_table()
-
-            try:
-                target_app = PopularNews.get(PopularNews.app_id == item['app_id'])
-
-                if target_app.discount != item['discount']:
-                    PopularNews.update(discount=item['discount']).where(PopularNews.app_id == item['app_id']).execute()
-
-                if target_app.final_price != item['final_price']:
-                    PopularNews.update(final_price=item['final_price']).where(
-                        PopularNews.app_id == item['app_id']).execute()
-
-            except PopularNews.DoesNotExist:
-                PopularNews.create(app_id=item['app_id'], name=item['name'], thumb_url=item['thumb_url'],
-                                   released=item['released'], discount=item['discount'],
-                                   final_price=item['final_price'],
-                                   origin_price=item['origin_price'])
+        try:
+            self.switch[spider.name](item)
+        except KeyError as e:
+            pass
 
         return item
